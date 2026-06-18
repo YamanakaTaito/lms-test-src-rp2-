b10f062a370d0a951f5013e73427b7860d8a204b
t-yamanaka <t-yamanaka@192.168.1.15>
2026-06-18 17:20:38 +0900

    add:Case01.java ログイン画面への遷移テストコード追加
    WebElementを利用してタイトルの値とログインボタンの値を取得して期待値通りかどうか確認するテストを追加した。
    期待値
    タイトル：ログイン | LMS
    ログインボタン：ログイン

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,11 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +14,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import jp.co.sss.lms.ct.util.WebDriverUtils;
 
@@ -36,9 +43,18 @@ public class Case01 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
+	void test01() throws IOException {
 		// TODO ここに追加
 		WebDriverUtils.goTo("http://localhost:8080/lms");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		WebElement classSelecterElementBtn = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		assertEquals("ログイン", classSelecterElementBtn.getAttribute("value"));
+		Path path = Path.of("evidence\\");
+		if (!Files.exists(path)) {
+			Files.createDirectory(path);
+		}
+		WebDriverUtils.getEvidence(new Object() {
+		});
 
 	}
 
