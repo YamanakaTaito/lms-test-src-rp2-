1ed003d49dbf38d47635b37fbe5566d8ee3cf19d
t-yamanaka <t-yamanaka@192.168.1.15>
2026-06-18 17:48:11 +0900

    add:Case02.java ログイン失敗時のテストコード追加
    WebElementを利用して、不正なloginIdとpasswordを入力し、期待値通りかを確認した。
    入力値
    loginId：Studentfalse01
    password：StudentAA01
    
    期待値
    エラーメッセージ：* ログインに失敗しました。
    タイトル：ログイン | LMS
    ログインボタン：ログイン

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,11 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +14,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -36,13 +45,49 @@ public class Case02 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		WebDriverUtils.goTo("http://localhost:8080/lms");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		WebElement classSelecterBtnElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		assertEquals("ログイン", classSelecterBtnElement.getAttribute("value"));
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
-	void test02() {
+	void test02() throws IOException {
 		// TODO ここに追加
+
+		//loginIdタグを選択して、指定の値を入力
+		WebElement loginIdElement = webDriver.findElement(By.id("loginId"));
+		loginIdElement.clear();
+		loginIdElement.sendKeys("Studentfalse01");
+
+		//passwordタグを選択して、指定の値を入力
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("StudentAA01");
+
+		//入力後.btn.btn-primaryをCSSセレクターで選択して、click
+		WebElement classSelecterBtnElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		classSelecterBtnElement.click();
+
+		//エラーメッセージ取得して、チェック
+		WebElement errorMsgWebElement = webDriver.findElement(By.cssSelector(".help-inline.error"));
+		assertEquals("* ログインに失敗しました。", errorMsgWebElement.getText());
+
+		//スクリーンショットをとる
+		Path path = Path.of("evidence\\");
+		if (!Files.exists(path)) {
+			Files.createDirectory(path);
+		}
+		WebDriverUtils.getEvidence(new Object() {
+		});
+		//ページ遷移していないか（ログイン失敗か）確認
+		WebDriverUtils.goTo("http://localhost:8080/lms");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		classSelecterBtnElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		assertEquals("ログイン", classSelecterBtnElement.getAttribute("value"));
+
 	}
 
 }
