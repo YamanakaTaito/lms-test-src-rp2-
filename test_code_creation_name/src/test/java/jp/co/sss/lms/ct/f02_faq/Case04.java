cb48daf0b89edffc57d59f55a5145610bc22379e
t-yamanaka <t-yamanaka@192.168.1.15>
2026-06-19 13:45:00 +0900

    add:Case04.java
    ログイン成功後、「機能」メニューから「ヘルプ」リンクをクリックし、「ヘルプ」ページの「よくある質問」のリンクをクリックして、期待通りかを確認するテストを作成した。
    期待値
    コース詳細画面：「機能」‐「ヘルプ」クリック→「ヘルプ」ページ
    ヘルプ：「よくある質問」クリック→「よくある質問」ページ

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,12 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +15,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト よくある質問機能
@@ -36,6 +48,10 @@ public class Case04 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		WebDriverUtils.goTo("http://localhost:8080/lms");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		WebElement classSelecterBtnElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		assertEquals("ログイン", classSelecterBtnElement.getAttribute("value"));
 	}
 
 	@Test
@@ -43,6 +59,38 @@ public class Case04 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+
+		//ログイン前のスクリーンショットをとる
+		Path path = Path.of("evidence\\");
+		if (!Files.exists(path)) {
+			try {
+				Files.createDirectory(path);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		WebDriverUtils.getEvidence(new Object() {
+		}, "ログイン画面");
+		//loginIdタグを選択して、指定の値を入力
+		WebElement loginIdElement = webDriver.findElement(By.id("loginId"));
+		loginIdElement.clear();
+		loginIdElement.sendKeys("StudentAA01");
+		//passwordタグを選択して、指定の値を入力
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("studentsTestsAA0012");
+
+		//入力後.btn.btn-primaryをCSSセレクターで選択して、click
+		WebElement classSelecterBtnElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		classSelecterBtnElement.click();
+		//遷移後、ページ生成とテスト実行と差があるため待機
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.titleIs("コース詳細 | LMS"));
+		//ページ遷移したかタイトルで確認
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+		//ログイン後のスクリーンショットをとる。上でディレクトリチェックしたので省略
+		WebDriverUtils.getEvidence(new Object() {
+		}, "コース詳細画面");
 	}
 
 	@Test
@@ -50,6 +98,17 @@ public class Case04 {
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
 		// TODO ここに追加
+		//リストメニューから「ヘルプ」を選択
+		//リストメニューをクリック
+		WebElement menuTextElement = webDriver.findElement(By.linkText("機能"));
+		menuTextElement.click();
+
+		WebElement linkTextElement = webDriver.findElement(By.linkText("ヘルプ"));
+		linkTextElement.click();
+		//ページ遷移したかタイトルで確認
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());
+		WebDriverUtils.getEvidence(new Object() {
+		}, "ヘルプ");
 	}
 
 	@Test
@@ -57,6 +116,28 @@ public class Case04 {
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
 		// TODO ここに追加
+		//タブ切り替えがあるため、最初のページを基本の識別子とする
+		String main_window_handle = webDriver.getWindowHandle();
+		//よくある質問リンクをクリック
+		WebElement linkTextElement = webDriver.findElement(By.linkText("よくある質問"));
+		linkTextElement.click();
+		//タブが2つになる。最初のページ以外にアクセスする
+		for (String handle : webDriver.getWindowHandles()) {
+			if (!(main_window_handle == handle)) {
+				//新しいタブに移動する
+				webDriver.switchTo().window(handle);
+			}
+		}
+		//新しいタブに移動する
+		webDriver.switchTo().window(webDriver.getWindowHandle());
+		//遷移後、ページ生成とテスト実行と差があるため待機
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.titleIs("よくある質問 | LMS"));
+		//ページ遷移したかタイトルで確認
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());
+		WebDriverUtils.getEvidence(new Object() {
+		}, "よくある質問");
+
 	}
 
 }
