ddf2499c11ecb9e773ca1f508be3a44b3380a054
t-yamanaka <t-yamanaka@192.168.1.15>
2026-06-23 10:33:05 +0900

    fix: Case08.javaの受講生レポート修正（週報）用テストの追加
    「セクション詳細画面」へ遷移するところから、報告内容を以下入力値に修正し、「提出する」ボタンを押下する。「セクション詳細画面」に戻り、上部メニューのリンクからユーザー詳細画面に遷移して、該当レポートの「詳細」ボタンを押下しレポート詳細画面で修正内容が反映されていることを確認できるテストを作成した。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
@@ -153,6 +153,31 @@ public class Case08 {
 	@DisplayName("テスト05 報告内容を修正して「提出する」ボタンを押下しセクション詳細画面に遷移")
 	void test05() {
 		// TODO ここに追加
+		//定数化
+		final String TEXTAREA_TEXT = "content_1";
+		final String UPDATE_REPORT_TEXT = "週報のサンプルを変更しました。";
+		final String EVIDENCE_FILE_NAME_TEXTAREA = "テキストエリア入力";
+		final String SUBMIT_BUTTON_SELECTOR = ".btn.btn-primary";
+		final String EVIDENCE_FILE_NAME_BASE = "セクション詳細";
+		final String CHECK_TITLE = "セクション詳細 | LMS";
+		//textarea「所感」のテキストボックスを指定して、修正する
+		WebElement textElement = webDriver.findElement(By.id(TEXTAREA_TEXT));
+		textElement.clear();
+		textElement.sendKeys(UPDATE_REPORT_TEXT);
+		//textareaに入力したスクリーンショット
+		WebDriverUtils.scrollBy("300");
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_NAME_TEXTAREA);
+		//「提出」ボタンをクリックする。
+		webDriver.findElement(By.cssSelector(SUBMIT_BUTTON_SELECTOR)).click();
+		//検索実行とテスト実行と差があるため待機
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.titleIs(CHECK_TITLE));
+		//ページ遷移したかタイトルで確認
+		assertEquals(CHECK_TITLE, webDriver.getTitle());
+		//ページ遷移後のスクリーンショットをとる。
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_NAME_BASE);
 	}
 
 	@Test
@@ -160,6 +185,20 @@ public class Case08 {
 	@DisplayName("テスト06 上部メニューの「ようこそ○○さん」リンクからユーザー詳細画面に遷移")
 	void test06() {
 		// TODO ここに追加
+		//定数化
+		final String LINK_TEXT = "ようこそ受講生ＡＡ１さん";
+		final String EVIDENCE_FILE_NAME_BASE = "ユーザー詳細";
+		final String CHECK_TITLE = "ユーザー詳細";
+		//ようこそ○○さんリンクをクリックする
+		webDriver.findElement(By.linkText(LINK_TEXT)).click();
+		//検索実行とテスト実行と差があるため待機
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.titleIs(CHECK_TITLE));
+		//ページ遷移したかタイトルで確認
+		assertEquals(CHECK_TITLE, webDriver.getTitle());
+		//ページ遷移後のスクリーンショットをとる。
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_NAME_BASE);
 	}
 
 	@Test
@@ -167,6 +206,28 @@ public class Case08 {
 	@DisplayName("テスト07 該当レポートの「詳細」ボタンを押下しレポート詳細画面で修正内容が反映される")
 	void test07() {
 		// TODO ここに追加
+		//定数化
+		final String DEFAULT_BUTTON_VALUE = "詳細";
+		final String UPDATE_REPORT_TEXT = "週報のサンプルを変更しました。";
+		final String EVIDENCE_FILE_NAME_BASE = "レポート修正内容の反映";
+		//週報の詳細をクリックする
+		List<WebElement> submitElements = webDriver
+				.findElements(By.cssSelector("Input[value=" + DEFAULT_BUTTON_VALUE + "] "));
+
+		//
+		WebDriverUtils.scrollBy("300");
+
+		//対象のsubmitは２つ目。
+		submitElements.get(1).click();
+		//対象の「所感」はテーブルタグ３つ目のtdタグ２つ目の中にある
+		List<WebElement> tableElements = webDriver.findElements(By.tagName("table"));
+		List<WebElement> tdElements = tableElements.get(2).findElements(By.tagName("td"));
+		//	「週報のサンプルを変更しました。」と比較
+		assertEquals(UPDATE_REPORT_TEXT, tdElements.get(1).getText());
+		//修正内容が確認できたことのスクリーンショット
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_NAME_BASE);
+
 	}
 
 }
