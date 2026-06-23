52a51fa53e1b08e9a44119426c3ee6d22147c845
t-yamanaka <t-yamanaka@192.168.1.15>
2026-06-23 11:06:12 +0900

    add:Case15.java 初回ログイン、利用規約に不同意
    初回ログインのアカウントでログイン。セキュリティ規約のページで利用規約に同意するチェックボックスを未入力で、次へボタンをクリックするとエラーメッセージが表示されるという期待通りの動きがあるか確認するテストを作成した。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
@@ -1,6 +1,12 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +15,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能②
@@ -36,6 +48,27 @@ public class Case15 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		//定数化
+		final String EVIDENCE_DIR_PATH = "evidence\\";
+		final String CHECK_TITLE = "ログイン | LMS";
+		final String LOGIN_BUTTON_TEXT = "ログイン";
+		final String EVIDENCE_FILE_NAME_BASE = "ログイン画面";
+
+		WebDriverUtils.goTo("http://localhost:8080/lms");
+		assertEquals(CHECK_TITLE, webDriver.getTitle());
+		WebElement classSelecterBtnElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		assertEquals(LOGIN_BUTTON_TEXT, classSelecterBtnElement.getAttribute("value"));
+		//ログイン画面のスクリーンショットをとる
+		Path path = Path.of(EVIDENCE_DIR_PATH);
+		if (!Files.exists(path)) {
+			try {
+				Files.createDirectory(path);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_NAME_BASE);
 	}
 
 	@Test
@@ -43,6 +76,32 @@ public class Case15 {
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		//入力値を定数で用意
+		final String LOGIN_ID = "StudentAA02";
+		final String PASSWORD = "StudentAA02";
+		final String CHECK_TITLE = "セキュリティ規約 | LMS";
+		final String EVIDENCE_FILE_NAME_BASE = "セキュリティ規約";
+
+		//loginIdタグを選択して、指定の値を入力
+		WebElement loginIdElement = webDriver.findElement(By.id("loginId"));
+		loginIdElement.clear();
+		loginIdElement.sendKeys(LOGIN_ID);
+		//passwordタグを選択して、指定の値を入力
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys(PASSWORD);
+
+		//入力後.btn.btn-primaryをCSSセレクターで選択して、click
+		WebElement classSelecterBtnElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		classSelecterBtnElement.click();
+		//遷移後、ページ生成とテスト実行と差があるため待機
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.titleIs(CHECK_TITLE));
+		//ページ遷移したかタイトルで確認
+		assertEquals(CHECK_TITLE, webDriver.getTitle());
+		//ログイン後のスクリーンショットをとる。
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_NAME_BASE);
 	}
 
 	@Test
@@ -50,6 +109,17 @@ public class Case15 {
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックをせず「次へ」ボタンを押下")
 	void test03() {
 		// TODO ここに追加
-	}
+		//定数化
+		final String SUBMIT_BUTTON_SELECTOR = ".btn.btn-primary";
+		final String ERROR_SELECTER = "error";
+		final String EVIDENCE_FILE_NAME_BASE = "エラーメッセージ表示";
+		//「次へ」ボタンをクリックする。
+		webDriver.findElement(By.cssSelector(SUBMIT_BUTTON_SELECTOR)).click();
+		//errormessageが表示されることを確認する
+		assertTrue(webDriver.findElement(By.className(ERROR_SELECTER)).isDisplayed());
 
+		//ログイン後のスクリーンショットをとる。
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_NAME_BASE);
+	}
 }
