48acd7f91394c66a618c6493ffb7e910d0327721
t-yamanaka <t-yamanaka@192.168.1.15>
2026-06-25 16:16:39 +0900

    add:Case16.java 受講生初回ログインパスワード変更入力チェック
    未ログインのIDでログイン後、セキュリティ規約を同意した後、以下の入力値で新規パスワード登録を実行。バリデーションチェックによりエラーメッセージが表示されて、登録できないという期待通りに動くか確認するテストを作成した。
    「現在のパスワードが未入力」
    【入力値】
    現在のパスワード：未入力
    新しいパスワード：TestPassWord0801
    確認パスワード：TestPassWord0801
    「新しいパスワードが未入力」
    【入力値】
    現在のパスワード：StudentAA02
    新しいパスワード：未入力
    確認パスワード：TestPassWord0801
    「「新しいパスワード」の最大値チェック(20文字)」
    【入力値】
    現在のパスワード：StudentAA02
    新しいパスワード：TestPassWord0801TestPassWord0801
    確認パスワード：TestPassWord0801TestPassWord0801
    「「新しいパスワード」の形式チェック」
    【入力値】
    ①英数字以外
    現在のパスワード：StudentAA02
    新しいパスワード：あああああああああ
    確認パスワード：あああああああああ
    ②英大文字のみ
    現在のパスワード：StudentAA02
    新しいパスワード：TESTPASSWORD
    確認パスワード：TESTPASSWORD
    ③英小文字のみ
    現在のパスワード：StudentAA02
    新しいパスワード：testpassword
    確認パスワード：testpassword
    ④数字のみ
    現在のパスワード：StudentAA02
    新しいパスワード：12345678910
    確認パスワード：12345678910
    ⑤英小文字大文字
    現在のパスワード：StudentAA02
    新しいパスワード：TestPassWord
    確認パスワード：TestPassWord
    ⑥英小文字数字
    現在のパスワード：StudentAA02
    新しいパスワード：testpassword1410123
    確認パスワード：testpassword1410123
    ⑦英大文字数字
    現在のパスワード：StudentAA02
    新しいパスワード：TESTPASSWORD1410123
    確認パスワード：TESTPASSWORD1410123
    「「確認パスワード」が異なる」
    【入力値】
    現在のパスワード：StudentAA02
    新しいパスワード：TestPassWord0801
    確認パスワード：TestPassWord0802

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case16.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case16.java
@@ -1,6 +1,12 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +15,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能②
@@ -36,6 +48,27 @@ public class Case16 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		//定数化
+		final String EVIDENCE_DIR_PATH = "evidence\\";
+		final String CHECK_TITLE = "ログイン | LMS";
+		final String LOGIN_BUTTON_TEXT = "ログイン";
+		final String EVIDENCE_FILE_NAME_BASE = "ログイン画面";
+
+		WebDriverUtils.goTo("http://localhost:8080/lms");
+		assertEquals(CHECK_TITLE, webDriver.getTitle());
+		WebElement classSelecterBtnElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		assertEquals(LOGIN_BUTTON_TEXT, classSelecterBtnElement.getAttribute("value"));
+		//ログイン画面のスクリーンショットをとる
+		Path path = Path.of(EVIDENCE_DIR_PATH);
+		if (!Files.exists(path)) {
+			try {
+				Files.createDirectory(path);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_NAME_BASE);
 	}
 
 	@Test
@@ -43,6 +76,32 @@ public class Case16 {
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		//定数化
+		final String LOGIN_ID = "StudentAA02";
+		final String PASSWORD = "StudentAA02";
+		final String CHECK_TITLE = "セキュリティ規約 | LMS";
+		final String EVIDENCE_FILE_NAME_BASE = "セキュリティ規約";
+
+		//loginIdタグを選択して、指定の値を入力
+		WebElement loginIdElement = webDriver.findElement(By.id("loginId"));
+		loginIdElement.clear();
+		loginIdElement.sendKeys(LOGIN_ID);
+		//passwordタグを選択して、指定の値を入力
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys(PASSWORD);
+
+		//入力後.btn.btn-primaryをCSSセレクターで選択して、click
+		WebElement classSelecterBtnElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		classSelecterBtnElement.click();
+		//遷移後、ページ生成とテスト実行と差があるため待機
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.titleIs(CHECK_TITLE));
+		//ページ遷移したかタイトルで確認
+		assertEquals(CHECK_TITLE, webDriver.getTitle());
+		//ログイン後のスクリーンショットをとる。
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_NAME_BASE);
 	}
 
 	@Test
@@ -50,6 +109,29 @@ public class Case16 {
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックを入れ「次へ」ボタン押下")
 	void test03() {
 		// TODO ここに追加
+		//定数化
+		final String SELECTOR_CHECK_BOX = "input[name=\"securityFlg\"]";
+		final String NEXT_BUTTON_SELECTOR = ".btn.btn-primary";
+		final String EVIDENCE_FILE_PASSWORD_CHECK = "チェックボックスにチェック後";
+		final String CHECK_TITLE = "パスワード変更 | LMS";
+		final String EVIDENCE_FILE_NAME_BASE = "パスワード変更";
+
+		//チェックボックスにチェックを入れる
+		webDriver.findElement(By.cssSelector(SELECTOR_CHECK_BOX)).click();
+		//スクリーンショットをとる。
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_PASSWORD_CHECK);
+		//次へボタンを押す
+		webDriver.findElement(By.cssSelector(NEXT_BUTTON_SELECTOR)).click();
+		//遷移後、ページ生成とテスト実行と差があるため待機
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.titleIs(CHECK_TITLE));
+		//ページ遷移したかタイトルで確認
+		assertEquals(CHECK_TITLE, webDriver.getTitle());
+		//スクリーンショットをとる。
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_NAME_BASE);
+
 	}
 
 	@Test
@@ -57,6 +139,59 @@ public class Case16 {
 	@DisplayName("テスト04 パスワードを未入力で「変更」ボタン押下")
 	void test04() {
 		// TODO ここに追加
+		//定数化
+		final String SELECTOR_CURRENT_PASSWORD_INPUT = "currentPassword";
+		final String SELECTOR_NEW_PASSWORD_INPUT = "password";
+		final String SELECTOR_CONFIRM_PASSWORD_INPUT = "passwordConfirm";
+
+		final String TEST_DATA_CURRENT_PASSWORD_EMPTY = "";
+		final String TEST_DATA_NEW_PASSWORD = "TestPassWord0801";
+		final String TEST_DATA_CONFIRM_PASSWORD = "TestPassWord0801";
+		final String SCROLL_PIXELS = "1000";
+		final String UPDATE_BUTTON_SELECTOR = "button[type=\"submit\"]";
+		final String ALELT_BUTTON_UPDATE_SELECTOR = "upd-btn";
+		final String ERROR_SELECTOR = "li span.help-inline.error";
+		final String ERROR_MSG_CURRENT_PASSWORD_REQUIRED = "現在のパスワードは必須です。";
+		final String EVIDENCE_FILE_PASSWORD_INPUT = "パスワード入力後画面";
+		final String EVIDENCE_FILE_PASSWORD_REQUIRED_ERROR = "パスワードを未入力で「変更」のエラー";
+
+		//現在のパスワード入力（未入力）
+		WebElement currentPasswordElement = webDriver.findElement(By.id(SELECTOR_CURRENT_PASSWORD_INPUT));
+		currentPasswordElement.clear();
+		currentPasswordElement.sendKeys(TEST_DATA_CURRENT_PASSWORD_EMPTY);
+		//新しいパスワード
+		WebElement newPasswordElement = webDriver.findElement(By.id(SELECTOR_NEW_PASSWORD_INPUT));
+		newPasswordElement.clear();
+		newPasswordElement.sendKeys(TEST_DATA_NEW_PASSWORD);
+		//確認パスワード
+		WebElement confirmPasswordElement = webDriver.findElement(By.id(SELECTOR_CONFIRM_PASSWORD_INPUT));
+		confirmPasswordElement.clear();
+		confirmPasswordElement.sendKeys(TEST_DATA_CONFIRM_PASSWORD);
+
+		//入力後のスクリーンショット
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_PASSWORD_INPUT);
+		//画面表示のためスクロール
+		WebDriverUtils.scrollBy(SCROLL_PIXELS);
+		//入力後変更ボタンクリック
+		webDriver.findElement(By.cssSelector(UPDATE_BUTTON_SELECTOR)).click();
+		//ダイアログの変更ボタンを取得
+		WebElement alertButtonElement = webDriver.findElement(By.id(ALELT_BUTTON_UPDATE_SELECTOR));
+		//ダイアログのボタンが押せるようになるまで待機
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(alertButtonElement));
+		//ダイアログの変更ボタンをクリックする。
+		alertButtonElement.click();
+		//エラーメッセージが出力されるのを待機
+		wait.until(
+				ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(ERROR_SELECTOR),
+						ERROR_MSG_CURRENT_PASSWORD_REQUIRED));
+		//アラートメッセージの取得
+		WebElement passwordErrorMessage = webDriver.findElement(By.cssSelector(ERROR_SELECTOR));
+		assertEquals(ERROR_MSG_CURRENT_PASSWORD_REQUIRED, passwordErrorMessage.getText());
+		//スクリーンショットをとる。
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_PASSWORD_REQUIRED_ERROR);
 	}
 
 	@Test
@@ -64,6 +199,58 @@ public class Case16 {
 	@DisplayName("テスト05 20文字以上の変更パスワードを入力し「変更」ボタン押下")
 	void test05() {
 		// TODO ここに追加
+		//定数化
+
+		final String SELECTOR_CURRENT_PASSWORD_INPUT = "currentPassword";
+		final String SELECTOR_NEW_PASSWORD_INPUT = "password";
+		final String SELECTOR_CONFIRM_PASSWORD_INPUT = "passwordConfirm";
+
+		final String TEST_DATA_CURRENT_PASSWORD = "StudentAA02";
+		final String TEST_DATA_NEW_PASSWORD_EXCEED_MAX = "TestPassWord0801TestPassWord0801";
+		final String TEST_DATA_CONFIRM_PASSWORD_EXCEED_MAX = "TestPassWord0801TestPassWord0801";
+		final String SCROLL_PIXELS = "1000";
+		final String UPDATE_BUTTON_SELECTOR = "button[type=\"submit\"]";
+		final String ALELT_BUTTON_UPDATE_SELECTOR = "upd-btn";
+		final String ERROR_SELECTOR = "li span.help-inline.error";
+		final String ERROR_MSG_CURRENT_PASSWORD_LENGTH_EXCEEDS_MAX = "パスワードの長さが最大値(20)を超えています。";
+		final String EVIDENCE_FILE_PASSWORD_INPUT = "パスワード入力後画面";
+		final String EVIDENCE_FILE_PASSWORD_LENGTH_ERROR = "20文字以上の変更パスワードを入力し「変更」のエラー";
+		//現在のパスワード入力
+		WebElement currentPasswordElement = webDriver.findElement(By.id(SELECTOR_CURRENT_PASSWORD_INPUT));
+		currentPasswordElement.clear();
+		currentPasswordElement.sendKeys(TEST_DATA_CURRENT_PASSWORD);
+		//新しいパスワード最大値(20)を超え
+		WebElement newPasswordElement = webDriver.findElement(By.id(SELECTOR_NEW_PASSWORD_INPUT));
+		newPasswordElement.clear();
+		newPasswordElement.sendKeys(TEST_DATA_NEW_PASSWORD_EXCEED_MAX);
+		//確認パスワード最大値(20)を超え
+		WebElement confirmPasswordElement = webDriver.findElement(By.id(SELECTOR_CONFIRM_PASSWORD_INPUT));
+		confirmPasswordElement.clear();
+		confirmPasswordElement.sendKeys(TEST_DATA_CONFIRM_PASSWORD_EXCEED_MAX);
+		//入力後のスクリーンショット
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_PASSWORD_INPUT);
+		//画面表示のためスクロール
+		WebDriverUtils.scrollBy(SCROLL_PIXELS);
+		//入力後変更ボタンクリック
+		webDriver.findElement(By.cssSelector(UPDATE_BUTTON_SELECTOR)).click();
+		//ダイアログの変更ボタンを取得
+		WebElement alertButtonElement = webDriver.findElement(By.id(ALELT_BUTTON_UPDATE_SELECTOR));
+		//ダイアログのボタンが押せるようになるまで待機
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(alertButtonElement));
+		//ダイアログの変更ボタンをクリックする。
+		alertButtonElement.click();
+		//エラーメッセージが出力されるのを待機
+		wait.until(
+				ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(ERROR_SELECTOR),
+						ERROR_MSG_CURRENT_PASSWORD_LENGTH_EXCEEDS_MAX));
+		//アラートメッセージの取得
+		WebElement passwordErrorMessage = webDriver.findElement(By.cssSelector(ERROR_SELECTOR));
+		assertEquals(ERROR_MSG_CURRENT_PASSWORD_LENGTH_EXCEEDS_MAX, passwordErrorMessage.getText());
+		//スクリーンショットをとる。
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_PASSWORD_LENGTH_ERROR);
 	}
 
 	@Test
@@ -71,6 +258,58 @@ public class Case16 {
 	@DisplayName("テスト06 ポリシーに合わない変更パスワードを入力し「変更」ボタン押下")
 	void test06() {
 		// TODO ここに追加 
+		//定数化
+		final String SELECTOR_CURRENT_PASSWORD_INPUT = "currentPassword";
+		final String SELECTOR_NEW_PASSWORD_INPUT = "password";
+		final String SELECTOR_CONFIRM_PASSWORD_INPUT = "passwordConfirm";
+		final String TEST_DATA_CURRENT_PASSWORD = "StudentAA02";
+		final String SCROLL_PIXELS = "1000";
+		final String UPDATE_BUTTON_SELECTOR = "button[type=\"submit\"]";
+		final String ALELT_BUTTON_UPDATE_SELECTOR = "upd-btn";
+		final String ERROR_SELECTOR = "li span.help-inline.error";
+		final String ERROR_MSG_CURRENT_PASSWORD_LENGTH_EXCEEDS_MAX = "「パスワード」には半角英数字のみ使用可能です。"
+				+ "また、半角英大文字、半角英小文字、数字を含めた8～20文字を入力してください。";
+
+		final String EVIDENCE_FILE_PASSWORD_LENGTH_ERROR = "20文字以上の変更パスワードを入力し「変更」のエラー";
+
+		//規約違反のパスワードを配列
+		String[] invalidPasswordTests = { "あああああああああ", "TESTPASSWORD", "testpassword", "12345678910", "TestPassWord",
+				"testpassword1410123", "TESTPASSWORD1410123" };
+		for (String invalidPasswordTest : invalidPasswordTests) {
+			//現在のパスワード入力
+			WebElement currentPasswordElement = webDriver.findElement(By.id(SELECTOR_CURRENT_PASSWORD_INPUT));
+			currentPasswordElement.clear();
+			currentPasswordElement.sendKeys(TEST_DATA_CURRENT_PASSWORD);
+			//新しいパスワード
+			WebElement newPasswordElement = webDriver.findElement(By.id(SELECTOR_NEW_PASSWORD_INPUT));
+			newPasswordElement.clear();
+			newPasswordElement.sendKeys(invalidPasswordTest);
+			//確認パスワード
+			WebElement confirmPasswordElement = webDriver.findElement(By.id(SELECTOR_CONFIRM_PASSWORD_INPUT));
+			confirmPasswordElement.clear();
+			confirmPasswordElement.sendKeys(invalidPasswordTest);
+			//画面表示のためスクロール
+			WebDriverUtils.scrollBy(SCROLL_PIXELS);
+			//入力後変更ボタンクリック
+			webDriver.findElement(By.cssSelector(UPDATE_BUTTON_SELECTOR)).click();
+			//ダイアログの変更ボタンを取得
+			WebElement alertButtonElement = webDriver.findElement(By.id(ALELT_BUTTON_UPDATE_SELECTOR));
+			//ダイアログのボタンが押せるようになるまで待機
+			final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+			wait.until(ExpectedConditions.elementToBeClickable(alertButtonElement));
+			//ダイアログの変更ボタンをクリックする。
+			alertButtonElement.click();
+			//エラーメッセージが出力されるのを待機
+			wait.until(
+					ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(ERROR_SELECTOR),
+							ERROR_MSG_CURRENT_PASSWORD_LENGTH_EXCEEDS_MAX));
+			//アラートメッセージの取得
+			WebElement passwordErrorMessage = webDriver.findElement(By.cssSelector(ERROR_SELECTOR));
+			assertEquals(ERROR_MSG_CURRENT_PASSWORD_LENGTH_EXCEEDS_MAX, passwordErrorMessage.getText());
+			//スクリーンショットをとる。
+			WebDriverUtils.getEvidence(new Object() {
+			}, EVIDENCE_FILE_PASSWORD_LENGTH_ERROR + "_" + invalidPasswordTest + "_");
+		}
 	}
 
 	@Test
@@ -78,6 +317,58 @@ public class Case16 {
 	@DisplayName("テスト07 一致しない確認パスワードを入力し「変更」ボタン押下")
 	void test07() {
 		// TODO ここに追加
+		//定数化
+		final String SELECTOR_CURRENT_PASSWORD_INPUT = "currentPassword";
+		final String SELECTOR_NEW_PASSWORD_INPUT = "password";
+		final String SELECTOR_CONFIRM_PASSWORD_INPUT = "passwordConfirm";
+		final String TEST_DATA_CURRENT_PASSWORD = "StudentAA02";
+		final String TEST_DATA_NEW_PASSWORD = "TestPassWord0801";
+		final String TEST_DATA_CONFIRM_PASSWORD_MISMATCH = "TestPassWord0802";
+
+		final String EVIDENCE_FILE_PASSWORD_INPUT = "パスワード入力後画面";
+		final String SCROLL_PIXELS = "1000";
+		final String UPDATE_BUTTON_SELECTOR = "button[type=\"submit\"]";
+		final String ALELT_BUTTON_UPDATE_SELECTOR = "upd-btn";
+		final String ERROR_SELECTOR = "li span.help-inline.error";
+		final String ERROR_MSG_CURRENT_PASSWORD_MISMATCH = "パスワードと確認パスワードが一致しません。";
+		final String EVIDENCE_FILE_PASSWORD_MISMATCH_ERROR = "一致しない確認パスワードを入力のエラー";
+
+		//現在のパスワード入力
+		WebElement currentPasswordElement = webDriver.findElement(By.id(SELECTOR_CURRENT_PASSWORD_INPUT));
+		currentPasswordElement.clear();
+		currentPasswordElement.sendKeys(TEST_DATA_CURRENT_PASSWORD);
+		//新しいパスワード
+		WebElement newPasswordElement = webDriver.findElement(By.id(SELECTOR_NEW_PASSWORD_INPUT));
+		newPasswordElement.clear();
+		newPasswordElement.sendKeys(TEST_DATA_NEW_PASSWORD);
+		//確認パスワード間違い
+		WebElement confirmPasswordElement = webDriver.findElement(By.id(SELECTOR_CONFIRM_PASSWORD_INPUT));
+		confirmPasswordElement.clear();
+		confirmPasswordElement.sendKeys(TEST_DATA_CONFIRM_PASSWORD_MISMATCH);
+		//入力後のスクリーンショット
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_PASSWORD_INPUT);
+		//画面表示のためスクロール
+		WebDriverUtils.scrollBy(SCROLL_PIXELS);
+		//入力後変更ボタンクリック
+		webDriver.findElement(By.cssSelector(UPDATE_BUTTON_SELECTOR)).click();
+		//ダイアログの変更ボタンを取得
+		WebElement alertButtonElement = webDriver.findElement(By.id(ALELT_BUTTON_UPDATE_SELECTOR));
+		//ダイアログのボタンが押せるようになるまで待機
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(alertButtonElement));
+		//ダイアログの変更ボタンをクリックする。
+		alertButtonElement.click();
+		//エラーメッセージが出力されるのを待機
+		wait.until(
+				ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(ERROR_SELECTOR),
+						ERROR_MSG_CURRENT_PASSWORD_MISMATCH));
+		//アラートメッセージの取得
+		WebElement passwordErrorMessage = webDriver.findElement(By.cssSelector(ERROR_SELECTOR));
+		assertEquals(ERROR_MSG_CURRENT_PASSWORD_MISMATCH, passwordErrorMessage.getText());
+		//スクリーンショットをとる。
+		WebDriverUtils.getEvidence(new Object() {
+		}, EVIDENCE_FILE_PASSWORD_MISMATCH_ERROR);
 	}
 
 }
